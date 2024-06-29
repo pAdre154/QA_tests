36c1bbb76efe595fec30928f622812d598956178
Petr_Osipov <petr.osipov72@gmail.com>
2024-06-29 23:46:54 +0300

    included dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,24 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    // RestAssured
+    val restAssuredVersion = "5.4.0"
+    testImplementation("io.rest-assured:rest-assured:$restAssuredVersion")
+    testImplementation("io.rest-assured:json-path:$restAssuredVersion")
+    testImplementation("io.rest-assured:json-schema-validator:$restAssuredVersion")
+    testImplementation("io.rest-assured:spring-mock-mvc:$restAssuredVersion")
+    testImplementation("io.rest-assured:kotlin-extensions:$restAssuredVersion")
+
+    // JUnit
+    val jUnitVersion = "5.8.1"
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+
+    // Jackson
+    val jacksonVersion = "2.0.1"
+    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+    implementation("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
 }
 
 tasks.test {
